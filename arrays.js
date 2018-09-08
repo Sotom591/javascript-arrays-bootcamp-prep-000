b600eec2ea9eac48c469e6cba1468ea1594c81cb
Marissa So <sotom591@gmail.com>
2018-09-08 00:13:58 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,63 @@
+var ingredient1 = "bread";
+var ingredient2 = "mild cheese";
+var ingredient3 = "sharp cheese";
+var ingredient4 = "butter";
+var ingredient5 = "tomato";
+var ingredient6 = "garlic";
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+
+const array = [1, 2, 3, 4];
+
+function addElementToBeginningOfArray(){
+var randomArray = ["Thing1", "Thing2"];
+["Thing01", ...randomArray];
+}
+addElementToBeginningOfArray;
+
+function destuctivelyAddElementToBeginningOfArray(Thing0){
+randomArray.unshift("Thing0");
+}
+destructivelyAddElementToEndOfArray;
+
+function addElementToEndOfArray(Thing03){
+[...randomArray, "Thing03"];  
+}
+addElementToEndOfArray;
+
+function destructivelyAddElementToEndOfArray(Thing3){
+randomArray.push("Thing3");
+}
+
+
+function accessElementInArray(Bananas){
+  var arrayList = ["Art", "Bananas", "Cats", "Droplets", "Europe"];
+  console.log (arrayList[1]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+const meses = ["Enero", "Febrero", "Marzo", "Abril"];
+meses.shift();
+meses;
+}
+
+function removeElementFromBeginningOfArray(){
+  var colors = ["Blue", "Yellow", "Green", "Red"];
+  colors.slice(1);
+  colors;
+}
+
+function destructivelyRemoveElementFromEndOfArray(){
+  var girlsNames = ["Jill", "Sarah", "Eliza"];
+  girlsNames.pop();
+  girlsNames;
+}
+
+ function removeElementFromEndOfArray(array) {
+   
+ }
+function removeElementFromEndOfArray(array){
+var pizzaToppings = ["pepperoni", "sausage", "olives", "pineapple"];
+pizzaToppings.slice(0, pizzaToppings.length - 1);
+}
